7f17d232508763cf1993803555e61d9d41ecd1b4
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-04-12 20:06:01 +0200

    Ported SoundCatalogue

---
+++ Assets/_ModContent/FlowerButton/SoundCatalogue.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace FlowerButtonMod.FlowerButton {
+
+	public static class SoundCatalogue {
+		public const string Press = "snd_flowerButtonPress";
+		public const string Release = "snd_flowerButtonRelease";
+		public const string VoOopsLaughter = "snd_flowerButton_VO_oops";
+
+		public const string MusicBoxMusic = "mus_monodrama_music_box";
+	}
+
+}
