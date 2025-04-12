42de3974fa176fe4c14993bca026cf7f7bb199c1
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-04-12 20:24:31 +0200

    Copied over BombTimerSapper

---
+++ Assets/_ModContent/FlowerButton/BombTimerSapper.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using FlowerButtonMod.TimerModCompatibility;
+using TimeSpan = System.TimeSpan;
+
+
+namespace FlowerButtonMod.FlowerButton {
+
+	internal class BombTimerSapper {
+
+		public BombTimerSapper(KMBombModule sappingModule) {
+			this.sappingModule = sappingModule;
+		}
+
+		readonly KMBombModule sappingModule;
+
+		object cachedBombReference = null;
+		object cachedTimerComponentDisplayTextMeshPro = null;
+
+		/// <returns>true if could sap the timer</returns>
+		public bool SapBombTimerForOneFrame(string displayOverride) {
+
+			if (Application.isEditor) {
+
+				// Override time in test harness
+				GameObject
+					.Find("Bomb/TimerModule(Clone)/Timer_Screen/Timer Text")
+					.GetComponent<TextMesh>()
+					.text = displayOverride;
+
+				return true;
+			};
+
+			// Find the bomb
+			if (cachedBombReference == null) {
+				cachedBombReference = sappingModule
+									   ?.GetComponent("BombComponent")
+									   ?.GetValue<object>("Bomb");
+
+				if (cachedBombReference == null) return false;
+			}
+
+			// Find the timer text
+			if (cachedTimerComponentDisplayTextMeshPro == null) {
+				cachedTimerComponentDisplayTextMeshPro = cachedBombReference
+										?.CallMethod<object>("GetTimer")
+										?.GetValue<object>("text");
+
+				if (cachedTimerComponentDisplayTextMeshPro == null) return false;
+			}
+
+			// Override for 1 frame
+			cachedTimerComponentDisplayTextMeshPro.SetValue("text", displayOverride);
+
+			// Also give notice to the patcher
+			var compatibilityService = Object.FindObjectOfType<TimerModCompatibilityService>();
+			if (compatibilityService == null) return true;
+
+			TimerModifierPatcher.OverrideNewBombTimerForOneFrame(
+				cachedBombReference,
+				displayOverride
+			);
+
+			return true;
+		}
+
+		/// <returns>true if could subtract time</returns>
+		public bool SubtractTime(TimeSpan time) {
+
+			if (Application.isEditor) {
+
+				#if UNITY_EDITOR
+				// Subtract time in test harness
+				// TimerModule is not in the final assembly
+				TimerModule testHardnessTimer = GameObject
+													.Find("Bomb/TimerModule(Clone)")
+													.GetComponent<TimerModule>();
+
+				testHardnessTimer.TimeRemaining -= (float)time.TotalSeconds;
+				#endif
+
+				return true;
+			}
+
+			// Find the bomb
+			if (cachedBombReference == null) {
+				cachedBombReference = sappingModule
+									   ?.GetComponent("BombComponent")
+									   ?.GetValue<object>("Bomb");
+
+				if (cachedBombReference == null) return false;
+			}
+
+			// Find the timer
+			object timer = cachedBombReference?.CallMethod<object>("GetTimer");
+			if (timer == null) return false;
+			
+			// Perform additional subtraction
+			float newTime = timer.GetValue<float>("TimeRemaining") - (float)time.TotalSeconds;
+			timer.SetValue("TimeRemaining", newTime);
+
+			return true;
+		}
+
+	}
+
+}
