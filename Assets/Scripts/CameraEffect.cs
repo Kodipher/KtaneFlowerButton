0122a6fbc580cdf1c9ee99646c5c9698ed5cef04
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-04-12 18:03:38 +0200

    Added CameraEffect.cs

---
+++ Assets/Scripts/CameraEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// Original source: https://github.com/QuickzYT/camera-material-tutorial-source/blob/main/Assets/Scripts/CameraEffect.cs
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class CameraEffect : MonoBehaviour {
+
+	public Material material;
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		
+		if (material == null) {
+			Graphics.Blit(source, destination);
+			return;
+		}
+
+		Graphics.Blit(source, destination, material);
+
+	}
+
+}
