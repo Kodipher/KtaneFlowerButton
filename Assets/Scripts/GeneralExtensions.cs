3caaa0f16fc6d5679a26a2353f5165d5200cba23
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-04-12 18:02:05 +0200

    Added community scripts

--- Assets/Scripts/GeneralExtensions.cs
+++ Assets/Scripts/GeneralExtensions.cs
@@ -296,4 +296,19 @@ public static class GeneralExtensions
         }
         return -1;
     }
+
+	// From https://github.com/Timwi/KtaneDoubleOh/blob/master/Assets/Ut.cs
+	// Required by FakeStatusLight
+	public static Transform FindDeepChild(this Transform aParent, string aName) {
+		var result = aParent.Find(aName);
+		if (result != null)
+			return result;
+		foreach (Transform child in aParent) {
+			result = child.FindDeepChild(aName);
+			if (result != null)
+				return result;
+		}
+		return null;
+	}
+
 }
