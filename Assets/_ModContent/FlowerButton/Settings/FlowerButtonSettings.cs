1738caf11145fe0d3d07fc101e7026efe354f1e5
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-04-12 18:46:25 +0200

    Ported over module settings

---
+++ Assets/_ModContent/FlowerButton/Settings/FlowerButtonSettings.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+
+namespace FlowerButton.Settings {
+
+	/// <summary>
+	/// A settings "data" class containing user configuarion of this mod.
+	/// Is not immutable but also is not global.
+	/// </summary>
+	public class FlowerButtonSettings {
+
+		public const int CurrentVersion = 1;
+
+		[JsonProperty("readme", Required = Required.Default, Order = -2)]
+		public string readme = "Note: These settings are meant for accessibility";
+
+		[JsonProperty("version", Required = Required.Always, Order = -1)]
+		public int version = CurrentVersion;
+
+		[JsonProperty("disable_forced_detonation")]
+		public bool disableForcedDetonation = false;
+
+		[JsonProperty("disable_visual_distortion")]
+		public bool disableVisualDistortion = false;
+
+		[JsonProperty("disable_musicbox")]
+		public bool disableMusicbox = false;
+
+	}
+
+}
---
+++ Assets/_ModContent/FlowerButton/Settings/SettingsReader.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace FlowerButton.Settings {
+
+
+	/// <summary>
+	/// Reader of <see cref="FlowerButtonSettings"/> from disk.
+	/// </summary>
+	public static class SettingsReader {
+
+		readonly static ModConfig<FlowerButtonSettings> modconfig;
+
+		static SettingsReader() {
+
+			// Create the inner reader
+			modconfig = new ModConfig<FlowerButtonSettings>("FlowerButtonSettings");
+
+			// Check the current save
+			FlowerButtonSettings read = modconfig.Read();
+
+			// If it is invalid -- start anew
+			if (!modconfig.SuccessfulRead) {
+				modconfig.SuccessfulRead = true;	// force write to work
+				modconfig.Write(new FlowerButtonSettings());
+			}
+
+			// If settings are outdated -- resave with new version
+			if (read.version < FlowerButtonSettings.CurrentVersion) {
+				read.version = FlowerButtonSettings.CurrentVersion;
+				modconfig.Write(read);
+			}
+		}
+
+		/// <summary>
+		/// Tries to read <see cref="FlowerButtonSettings"/> from mod configurations.
+		/// </summary>
+		/// <remarks>
+		/// Internally uses <see cref="ModConfig{T}.Read"/>.
+		/// </remarks>
+		/// <returns>
+		/// A new <see cref="FlowerButtonSettings"/> object.
+		/// </returns>
+		public static FlowerButtonSettings ReadSettings() {
+			return modconfig.Read();
+		}
+
+	}
+
+}
